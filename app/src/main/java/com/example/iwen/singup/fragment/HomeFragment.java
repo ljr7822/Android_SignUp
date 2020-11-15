89d6d5c0536ff4208e7bd640d0a8f519ecfd050f
iwenLjr <1816807688@qq.com>
2020-11-15 09:44:20 +0800

    图片选择器

--- app/src/main/java/com/example/iwen/singup/fragment/HomeFragment.java
+++ app/src/main/java/com/example/iwen/singup/fragment/HomeFragment.java
@@ -1,9 +1,14 @@
 package com.example.iwen.singup.fragment;
 
 import com.example.iwen.common.app.BaseFragment;
+import com.example.iwen.common.widget.GalleryView;
 import com.example.iwen.singup.R;
 
+import butterknife.BindView;
+
 public class HomeFragment extends BaseFragment {
+    @BindView(R.id.galleryView)
+    GalleryView mGalley;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -12,5 +17,16 @@ public class HomeFragment extends BaseFragment {
     @Override
     protected int getContentLayoutId() {
         return R.layout.fragment_home;
+    }
+
+    @Override
+    protected void initData() {
+        super.initData();
+        mGalley.setup(getLoaderManager(), new GalleryView.SelectedChangeListener() {
+            @Override
+            public void onSelectedCountChanged(int count) {
+
+            }
+        });
     }
 }
